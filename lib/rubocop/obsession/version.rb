255ce77fd9086f262564bf5e0b2166110807cfe9
Jerome Dalbert <jerome.dalbert@gmail.com>
2024-10-20 10:53:26 -0700

    Initial commit

---
+++ lib/rubocop/obsession/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module Obsession
+    VERSION = '0.1.0'
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'rubocop/obsession'
+
+Dir["#{__dir__}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end

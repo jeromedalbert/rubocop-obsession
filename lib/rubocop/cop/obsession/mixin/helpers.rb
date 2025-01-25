bfc936016f9618c3a37a5b1bef71e86c2a85c984
Jerome Dalbert <jerome.dalbert@gmail.com>
2025-01-25 11:24:48 -0800

    Move mixins to obsession namespace to avoid conflicts

---
+++ lib/rubocop/cop/obsession/mixin/helpers.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Obsession
+      module Helpers
+        def rails_callback?(callback)
+          return true if callback == 'validate'
+
+          callback.match?(
+            /
+            ^(before|after|around)
+            _.*
+            (action|validation|create|update|save|destroy|commit|rollback)$
+            /x
+          )
+        end
+
+        def verb?(string)
+          short_string = string[2..] if string.start_with?('re')
+
+          verbs.include?(string) || verbs.include?(short_string)
+        end
+
+        private
+
+        def verbs
+          @@verbs ||= File.read("#{__dir__}/files/verbs.txt").split
+        end
+      end
+    end
+  end
+end
--- lib/rubocop/obsession.rb
+++ lib/rubocop/obsession.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/string/inflections'
 require 'rubocop'
 
-require_relative 'cop/mixin/helpers'
+require_relative 'cop/obsession/mixin/helpers'
 Dir["#{__dir__}/cop/obsession/**/*.rb"].sort.each { |file| require file }
 require_relative 'obsession/version'
 

a3575b45c2b5e4219f03838a2b156f9f16f2e64f
Jerome Dalbert <jerome.dalbert@gmail.com>
2025-06-28 12:01:14 -0700

    Add tests for Rails/ServiceName

---
+++ spec/rubocop/cop/obsession/rails/service_name_spec.rb
@@ -0,0 +1,71 @@
+describe RuboCop::Cop::Obsession::Rails::ServiceName, :config do
+  let(:cop_config) { { 'EnforcedStyle' => 'drill_down' } }
+
+  context 'when class name does not start with a verb' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        class BlogPostPopularityJob < ApplicationJob
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Service or Job name should start with a verb.
+          def perform
+          end
+        end
+      RUBY
+    end
+
+    it 'registers an offense when class has a nested namespace' do
+      expect_offense(<<~RUBY)
+        module Scheduled
+          class BlogPostPopularityJob < ApplicationJob
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Service or Job name should start with a verb.
+            def perform
+            end
+          end
+        end
+      RUBY
+    end
+
+    it 'registers an offense when class has a compact namespace' do
+      expect_offense(<<~RUBY)
+        class Scheduled::BlogPostPopularityJob < ApplicationJob
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Service or Job name should start with a verb.
+          def perform
+          end
+        end
+      RUBY
+    end
+
+    it 'does not register an offense when class has more than 1 public method' do
+      expect_no_offenses(<<~RUBY)
+        class BlogPostImporter < ApplicationJob
+          def import_one_post
+          end
+
+          def import_all_posts
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'when class name starts with a verb' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        class UpdateBlogPostPopularityJob < ApplicationJob
+          def perform
+          end
+        end
+      RUBY
+    end
+
+    it 'does not register an offense when class has a nested namespace' do
+      expect_no_offenses(<<~RUBY)
+        module Scheduled
+          class UpdateBlogPostPopularityJob < ApplicationJob
+            def perform
+            end
+          end
+        end
+      RUBY
+    end
+  end
+end
